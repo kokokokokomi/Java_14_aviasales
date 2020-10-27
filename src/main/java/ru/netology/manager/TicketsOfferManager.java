3cec52fefdff596447557b33277fe8eb18fbf8ab
komisuomi <only-for-serzh@yandex.ru>
2020-10-27 17:40:01 +0300

    Added Methods for TicketsOfferManager

--- src/main/java/ru/netology/manager/TicketsOfferManager.java
+++ src/main/java/ru/netology/manager/TicketsOfferManager.java
@@ -1,6 +1,31 @@
 package ru.netology.manager;
 import ru.netology.domain.TicketsOffer;
 import ru.netology.repository.TicketsOfferRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TicketsOfferManager {
+    private TicketsOffer[] items = new TicketsOffer[0];
+
+    public TicketsOffer findByDepartureAirport(String departureAirport) {
+            for (TicketsOffer item : items) {
+                if (item.getDepartureAirport() == departureAirport) {
+                    return item;
+                }
+            }
+            return null;
+    }
+
+    public TicketsOffer findByArrivalAirport(String arrivalAirport) {
+        for (TicketsOffer item : items) {
+            if (item.getArrivalAirport() == arrivalAirport) {
+                return item;
+            }
+        }
+        return null;
+    }
 }
